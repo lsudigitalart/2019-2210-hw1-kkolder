4c3e124c7e6e63de0f4cad1db094f3bc353e1795
Kase <54596979+kkolder@users.noreply.github.com>
2019-09-03 12:58:31 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+function setup(){
+    createCanvas(1265,670);
+background(0);
+//fill(255);
+strokeWeight(10);
+noStroke();
+ellipse(632,446,40,40); /**head */
+ellipse(632,475,20,20); /**neck */
+ellipse(632,510,50,50); /**chest */
+ellipse(632,540,46,46);
+ellipse(617,479,20,20); /**shoulders */
+ellipse(647,479,20,20);
+ellipse(664,483,20,20);
+ellipse(600,483,20,20);
+ellipse(610,490,20,20);
+ellipse(650,490,20,20);
+ellipse(596,515,20,75); /**arms */
+ellipse(668,515,20,75);
+ellipse(632,565,30,30); /**legs*/
+ellipse(620,595,22,120);
+ellipse(644,595,22,120);
+ellipse(200,100,100,100);/**cloud */
+ellipse(600,200,100,100);
+ellipse(800,230,100,100);
+ellipse(400,300,100,100);
+ellipse(632,150,10,500);
+ellipse(600,100,10,500);
+ellipse(700,80,10,500);
+ellipse(800,100,10,400);
+ellipse(800,140,10,400);
+ellipse(400,100,10,300);
+ellipse(100,100,10,200);
+ellipse(1000,100,10,300);
+ellipse(300,150,20,400);
+ellipse(600,150,20,400);
+ellipse(1100,150,15,400);
+ellipse(100,100,40,40)
+ellipse(340,200,40,40)
+ellipse(560,160,40,40)
+ellipse(720,200,40,40)
+ellipse(640,100,40,40)
+ellipse(890,300,40,40)
+ellipse(930,100,40,40)
+ellipse(1200,200,40,40)
+strokeWeight (30) /**BLACK CIRCLES */
+stroke(0)
+ellipse(600,200,50,50);
+ellipse(800,100,70,70);
+strokeWeight (20)
+stroke(0)
+ellipse(200,100,30,30);
+}
+function draw(){
+
+}
